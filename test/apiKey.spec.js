13add5a792acf244769606b9393910e6ed851044
olicrea <olimar.digital@gmail.com>
2024-03-13 18:15:44 -0300

    se hacen pruebas unitarias para getApiKey y setApiKey

---
+++ test/apiKey.spec.js
@@ -0,0 +1,23 @@
+import { getApiKey, setApiKey } from "../src/lib/apiKeyFunction";
+
+describe('getApiKey', () => {
+  
+  it('debería devolver el valor de la API Key', () => {
+    // Desarrolla el test correspondiente aquí
+    const key = "key-key";
+    localStorage.setItem('apiKey', key);
+    //console.log(getApiKey() === "key-key");
+    expect(getApiKey()).toBe("key-key");
+  });
+});
+
+describe('setApiKey', () => {
+
+  it('debería establecer correctamente la API Key', () => {
+    // Desarrolla el test correspondiente aquí
+    const newKey = "Nueva-key-key";
+    setApiKey(newKey);
+    //console.log(localStorage.getItem('apiKey') === "Nueva-key-key");
+    expect(localStorage.getItem('apiKey')).toBe(newKey);
+  });
+});
