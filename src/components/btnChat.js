3b72a1dbf25407816d81c395e563ed775bafdd0b
olicrea <olimar.digital@gmail.com>
2024-03-13 17:48:42 -0300

    se eliminan archivos btnChat.js y chatsTemplate.js, no son parte del boilerplate

--- src/components/btnChat.js
+++
@@ -1,7 +0,0 @@
-export const btnChat = () => {
-  const btnChat = document.createElement("button");
-  btnChat.className = "btn btn-chat";
-  const textBoton = document.createTextNode("Chatear");
-  btnChat.appendChild(textBoton);
-  return btnChat;
-};
