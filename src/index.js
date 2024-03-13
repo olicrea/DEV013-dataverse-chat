58b23601559651be2b7d923266ec7d43b5495d68
olicrea <olimar.digital@gmail.com>
2024-03-13 17:42:53 -0300

    en routes se agrega /groupChats, asociado a su propia vista

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import { home } from './views/home.js';
 import { viewAPIKey } from './views/viewAPIKey.js';
 import { viewBienvenida } from './views/viewBienvenida.js';
-import { viewChats } from './views/viewChats.js';
+import { viewGroupChat } from './views/viewGroupChat.js';
 import { setRoutes, setRootElement, onURLChange } from './router.js';
 import { viewError } from './views/viewError.js';
 import { viewDescriptionCard } from './views/viewDescriptionCard.js';
+import { viewIndividualChat } from './views/viewIndividualChat.js';
 
 const routes = {
   "/": home, //Home donde se ven todas las tarjetas
   "/error": viewError,
   "/viewAPIKey": viewAPIKey,
-  "/chats": viewChats,
+  "/chats": viewIndividualChat,
+  "/groupChats": viewGroupChat,
   "/description": viewDescriptionCard,
   "/confirmar": viewBienvenida,
 };
