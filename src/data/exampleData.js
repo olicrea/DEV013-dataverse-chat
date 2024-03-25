55bd10f42c7356b5cff1423c4c22ed6420220bc9
olicrea <olimar.digital@gmail.com>
2024-03-25 15:33:13 -0300

    agrego archivos de test functions

---
+++ src/data/exampleData.js
@@ -0,0 +1,58 @@
+export const data = [
+  {
+    "id": "hackers",
+    "name": "Hackers",
+    "shortDescription": "Jóvenes hackers y aventureros luchan contra la injusticia.",
+    "description": "Ambientada en el mundo del ciberespacio, sigue a un grupo de jóvenes hackers liderados por Dade Murphy (interpretado por Jonny Lee Miller) y Kate Libby (interpretada por Angelina Jolie). Juntos, se embarcan en una emocionante aventura para exponer una conspiración corporativa. Con una estética vibrante y una banda sonora electrónica, la película captura la subcultura hacker de los años 90. Hackers se destaca por su representación única de la ciberseguridad y la cultura hacker en una época de rápido avance tecnológico.",
+    "imageUrl": "https://raw.githubusercontent.com/MilenaPacheco/DEV013-dataverse/main/src/img/Hackers.png",
+    "facts": {
+      "yearMovie": 1995,
+      "directorMovie": "Iain Softley",
+      "productionCompany": "MGM",
+      "genreMovie": "Thriller",
+      "genreValue": "thriller"
+    },
+    "extraInfo": {
+      "rottenTomatoesScore": "32%",
+      "countryMovie": "Estados Unidos",
+      "durationMovie": "107 minutos"
+    }
+  },
+  {
+    "id": "short-circuit",
+    "name": "Short Circuit",
+    "shortDescription": "Un robot militar adquiere conciencia y busca la libertad.",
+    "description": "Es una encantadora comedia de ciencia ficción que sigue a Johnny 5, un robot militar experimental que adquiere conciencia propia después de ser alcanzado por un rayo. Huyendo del ejército y buscando descubrir el mundo, Johnny 5 se embarca en una aventura de autodescubrimiento. La película combina humor, ternura y un toque de acción mientras Johnny 5 forma amistades y desafía su programación original. Short Circuit ofrece una mirada entrañable a la inteligencia artificial y a la conexión humana, convirtiéndose en un clásico querido desde su lanzamiento en 1986.",
+    "imageUrl": "https://raw.githubusercontent.com/MilenaPacheco/DEV013-dataverse/main/src/img/short-circuit.png",
+    "facts": {
+      "yearMovie": 1986,
+      "directorMovie": "John Badham",
+      "productionCompany": "TriStar Pictures",
+      "genreMovie": "Ciencia ficción",
+      "genreValue": "fiction"
+    },
+    "extraInfo": {
+      "rottenTomatoesScore": "57%",
+      "countryMovie": "Estados Unidos",
+      "durationMovie": "98 minutos"
+    }
+  },
+  {
+    "id": "indie-game-the-movie",
+    "name": "Indie Game: The Movie",
+    "shortDescription": "Historias detrás de la creación de videojuegos indie.",
+    "description": "Sumerge a los espectadores en la vida de apasionados desarrolladores de videojuegos independientes. Esta obra proporciona una visión profunda de los desafíos emocionales y creativos que enfrentan mientras luchan por plasmar sus visiones artísticas en el exigente mundo de los videojuegos. A través de testimonios íntimos, la película captura momentos de triunfo y desesperación, revelando la dedicación incansable que impulsa a estos creadores. Desde la lucha contra limitaciones financieras hasta la búsqueda de reconocimiento, Indie Game: The Movie ofrece una mirada cautivadora a la compleja travesía de quienes buscan dejar su marca en la industria del juego independiente.",
+    "imageUrl": "https://raw.githubusercontent.com/MilenaPacheco/DEV013-dataverse/main/src/img/indie-game-the-movie.jpeg",
+    "facts": {
+      "yearMovie": 2012,
+      "directorMovie": "James Swirsky, Lisanne Pajot",
+      "productionCompany": "BlinkWorks Media",
+      "genreMovie": "Documental",
+      "genreValue": "documental"
+    },
+    "extraInfo": {
+      "rottenTomatoesScore": "93%",
+      "countryMovie": "Canadá",
+      "durationMovie": "96 minutos"
+    }
+  },];
